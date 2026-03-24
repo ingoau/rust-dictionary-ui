f287db67bea774476cead2d789a5b4e3fe9d68c9
Ingo Wolf <me@ingo.au>
2026-03-24 16:21:51 +1100

    update to just have get_defenition function

--- src/dict.rs
+++ src/dict.rs
@@ -1,5 +1,4 @@
 use serde::Deserialize;
-use std::env;
 
 #[derive(Deserialize)]
 struct MeaningDefinition {
@@ -28,47 +27,17 @@ struct Definition {
     meanings: Vec<Meaning>,
 }
 
-pub fn main() {
+pub fn get_defenition(word: &str) -> Vec<Definition> {
     let client = reqwest::blocking::Client::new();
 
-    let args: Vec<String> = env::args().collect();
+    let url = format!("https://api.dictionaryapi.dev/api/v2/entries/en/{word}");
 
-    if args.len() > 1 {
-        let word = &args[1];
+    let definition_response: Vec<Definition> = client
+        .get(url)
+        .send()
+        .expect("Failed to fetch definition")
+        .json()
+        .expect("Failed to parse response");
 
-        let url = format!("https://api.dictionaryapi.dev/api/v2/entries/en/{word}");
-
-        let definition_response: Vec<Definition> = client
-            .get(url)
-            .send()
-            .expect("Failed to fetch definition")
-            .json()
-            .expect("Failed to parse response");
-
-        for (i, word) in definition_response.iter().enumerate() {
-            println!("{}. {}", i + 1, word.word);
-
-            for (_, phonetic) in word.phonetics.iter().enumerate() {
-                if let Some(text) = &phonetic.text {
-                    println!("     {}", text)
-                }
-            }
-
-            for (_, meaning) in word.meanings.iter().enumerate() {
-                println!("   {}", meaning.part_of_speech);
-                for (i, def) in meaning.definitions.iter().enumerate() {
-                    println!("     {}. {}", i + 1, def.definition);
-                    if let Some(example) = &def.example {
-                        println!("        \"{}\"", example)
-                    }
-                }
-            }
-
-            for (_, source) in word.source_urls.iter().enumerate() {
-                println!("\n   {}\n\n", source)
-            }
-        }
-    } else {
-        println!("Input a word")
-    }
+    definition_response
 }
