5b97caf467c5492596e1bcdca83bd98ee548bdce
Ingo Wolf <me@ingo.au>
2026-03-24 18:08:29 +1100

    add landing page thingy

--- src/main.rs
+++ src/main.rs
@@ -43,58 +43,68 @@ impl DictApp {
     }
 
     fn search_ui(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame, ui: &mut egui::Ui) {
-        ui.horizontal(|ui| {
-            let text_input = ui.text_edit_singleline(&mut self.inputted_text);
-            if (ui
-                .button("Search ".to_owned() + egui_material_icons::icons::ICON_SEARCH)
-                .clicked()
-                || (text_input.lost_focus() && ui.input(|i| i.key_pressed(egui::Key::Enter))))
-                && !self.inputted_text.is_empty()
-            {
-                let definitions = dict::get_defenition(&self.inputted_text);
-                self.definitions = definitions;
-                text_input.request_focus();
-            }
-        });
+        let text_input = ui.text_edit_singleline(&mut self.inputted_text);
+        if (ui
+            .button("Search ".to_owned() + egui_material_icons::icons::ICON_SEARCH)
+            .clicked()
+            || (text_input.lost_focus() && ui.input(|i| i.key_pressed(egui::Key::Enter))))
+            && !self.inputted_text.is_empty()
+        {
+            let definitions = dict::get_defenition(&self.inputted_text);
+            self.definitions = definitions;
+            text_input.request_focus();
+        }
     }
 }
 
 impl eframe::App for DictApp {
     fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
         egui::CentralPanel::default().show(ctx, |ui| {
-            self.search_ui(ctx, _frame, ui);
-            ui.separator();
-            egui::ScrollArea::vertical()
-                .auto_shrink(false)
-                .show(ui, |ui| {
-                    for (i, def) in self.definitions.iter().enumerate() {
-                        let title = format!("{}. {}", i + 1, def.word);
-                        ui.push_id(title.clone(), |ui| {
-                            egui::Frame::default().inner_margin(16).show(ui, |ui| {
-                                ui.heading(title);
-                                ui.horizontal(|ui| {
-                                    for phonetic in &def.phonetics {
-                                        ui.label(phonetic.text.as_deref().unwrap_or(""));
-                                    }
-                                });
-                                for (_, meaning) in def.meanings.iter().enumerate() {
-                                    ui.collapsing(meaning.part_of_speech.clone(), |ui| {
-                                        for (i, definition) in
-                                            meaning.definitions.iter().enumerate()
-                                        {
-                                            ui.push_id(&i, |ui| {
-                                                ui.label(definition.definition.clone());
-                                                if let Some(example) = &definition.example {
-                                                    ui.label(example.clone());
-                                                }
-                                            });
+            if (self.definitions.is_empty()) {
+                ui.vertical_centered(|ui| {
+                    egui::Frame::default().inner_margin(16).show(ui, |ui| {
+                        ui.heading("Dictionary Thingy™");
+                        ui.label("Enter a word to search for its definition.");
+                        self.search_ui(ctx, _frame, ui);
+                    })
+                });
+            } else {
+                ui.horizontal(|ui| {
+                    self.search_ui(ctx, _frame, ui);
+                });
+                ui.separator();
+                egui::ScrollArea::vertical()
+                    .auto_shrink(false)
+                    .show(ui, |ui| {
+                        for (i, def) in self.definitions.iter().enumerate() {
+                            let title = format!("{}. {}", i + 1, def.word);
+                            ui.push_id(title.clone(), |ui| {
+                                egui::Frame::default().inner_margin(16).show(ui, |ui| {
+                                    ui.heading(title);
+                                    ui.horizontal(|ui| {
+                                        for phonetic in &def.phonetics {
+                                            ui.label(phonetic.text.as_deref().unwrap_or(""));
                                         }
                                     });
-                                }
+                                    for (_, meaning) in def.meanings.iter().enumerate() {
+                                        ui.collapsing(meaning.part_of_speech.clone(), |ui| {
+                                            for (i, definition) in
+                                                meaning.definitions.iter().enumerate()
+                                            {
+                                                ui.push_id(&i, |ui| {
+                                                    ui.label(definition.definition.clone());
+                                                    if let Some(example) = &definition.example {
+                                                        ui.label(example.clone());
+                                                    }
+                                                });
+                                            }
+                                        });
+                                    }
+                                });
                             });
-                        });
-                    }
-                });
+                        }
+                    });
+            }
         });
     }
 }
