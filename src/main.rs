745a09e84d96de3a4dbb570fc42e3050c2305ef3
Ingo Wolf <me@ingo.au>
2026-03-24 17:17:34 +1100

    add search icon

--- src/main.rs
+++ src/main.rs
@@ -24,7 +24,9 @@ impl eframe::App for DictApp {
         egui::CentralPanel::default().show(ctx, |ui| {
             ui.horizontal(|ui| {
                 let text_input = ui.text_edit_singleline(&mut self.inputted_text);
-                if (ui.button("Search").clicked()
+                if (ui
+                    .button("Search ".to_owned() + egui_material_icons::icons::ICON_SEARCH)
+                    .clicked()
                     || (text_input.lost_focus() && ui.input(|i| i.key_pressed(egui::Key::Enter))))
                     && !self.inputted_text.is_empty()
                 {
